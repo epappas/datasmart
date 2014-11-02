7d43d8f1e81aed8d6febf46911163fec2bcf7f8f
Evangelos Pappas <evangelos.pappas@matchesfashion.com>
2014-11-02 11:01:23 +0000

    datasmart_erl; rest_files: Stream uploading file to couch, no TMP files

--- datasmart_erl/lib/rest_files/src/files_handler.erl
+++ datasmart_erl/lib/rest_files/src/files_handler.erl
@@ -27,25 +27,36 @@ process(<<"GET">>, Req) ->
   end;
 
 process(<<"POST">>, Req) ->
+  {OUkeyBin, Req2} = cowboy_req:binding(oukey, Req),
 
-  case cowboy_req:parse_header(<<"content-type">>, Req) of
-    {ok, {<<"multipart">>, <<"form-data">>, _}, _Req} ->
-      {ok, Req1, FieldList, FileList} = multipart(Req),
+  case OUkeyBin of
+    undefined -> end_with_failure(400, "No Valid Arguments", Req2);
+    OUkeyBin ->
+      OUkey = binary:bin_to_list(OUkeyBin),
+      {ok, Ukey} = user_server:match_ouKey(OUkey),
+      case user_server:match_ouKey(OUkey) of
+        {ok, Ukey} ->
+          case cowboy_req:parse_header(<<"content-type">>, Req2) of
+            {ok, {<<"multipart">>, <<"form-data">>, _}, Req3} ->
 
-      echo(200, jiffy:encode({[
-        {fieldList, {FieldList}},
-        {fileList, {FileList}}
-      ]}), Req1);
+              {ok, Req4, FieldList, FileList} = multipart(Ukey, Req3),
 
-    _ -> end_with_failure(400, <<"Bad content-type; Accepts only multipart">>, Req)
+              echo(200, jiffy:encode({[
+                {fieldList, {FieldList}},
+                {fileList, {FileList}}
+              ]}), Req4);
+
+            _ -> end_with_failure(400, <<"Bad content-type; Accepts only multipart">>, Req)
+          end;
+        _ -> end_with_failure(400, "No Valid Arguments", Req2)
+      end
   end;
 
 process(_, Req) -> end_with_failure(405, "Method not allowed.", Req).
 
-handle_query({file_info, OUkey, FileKey}, Req) -> {info, [], Req};
+handle_query({file_info, _OUkey, _FileKey}, Req) -> {info, [], Req};
 
-handle_query({file_upload, OUkey}, Req) ->
-  {info, [], Req}.
+handle_query({file_upload, _OUkey}, Req) -> {info, [], Req}.
 
 echo(Status, Echo, Req) ->
   cowboy_req:reply(Status, [
@@ -60,16 +71,15 @@ end_with_failure(Code, Message, Req) ->
     {error, Message}
   ]}), Req).
 
-terminate(_Reason, _Req, _State) ->
-  ok.
+terminate(_Reason, _Req, _State) -> ok.
 
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
 
-multipart(Req) -> multipart(Req, [], []).
+multipart(UKey, Req) -> multipart(UKey, Req, [], []).
 
-multipart(Req, FieldList, FileList) ->
+multipart(UKey, Req, FieldList, FileList) ->
   case cowboy_req:part(Req) of
     {ok, Headers, Req2} ->
       {ok, Req4, NewFieldList, NewFileList} =
@@ -82,35 +92,66 @@ multipart(Req, FieldList, FileList) ->
             {ok, Req3, FieldList1, FileList};
           {file, FieldName, Filename, CType, CTransferEncoding} ->
 
-            {ok, TMPFname, Req3} = stream_file(Req2),
+            FileKey = lists:concat([
+              binary_to_list(UKey),
+              "_",
+              hash_md5:build(binary_to_list(Filename))
+            ]),
 
-            FileList1 = lists:append([{Filename, {[
-              {<<"fieldName">>, FieldName},
-              {<<"filename">>, Filename},
-              {<<"ctype">>, CType},
-              {<<"ctransferEncoding">>, CTransferEncoding},
-              {<<"tmpFile">>, TMPFname}
-            ]}}], FileList),
+            Opts =
+              case couch:get("user_uploads", FileKey) of
+                {ok, {FileKVList}} ->
+                  case proplists:get_value(<<"_rev">>, FileKVList) of
+                    undefined -> [];
+                    Rev -> [{rev, Rev}]
+                  end;
+                {error, {error, not_found}} -> []
+              end,
+
+            %% initiate attachmanet upload
+            {ok, UploadRef} = couch:attach("user_uploads", FileKey, {Filename, stream}, Opts),
+
+            %% commit upload
+            {ok, {ResultList}, Req3} = stream_file(Req2, UploadRef),
+
+            Rev1 = proplists:get_value(<<"rev">>, ResultList),
+            Id = proplists:get_value(<<"id">>, ResultList),
+
+            %% get current view
+            {ok, {DocKVList}} = couch:get("user_uploads", Id, Rev1),
+
+            %% append meta info
+            Doc = {lists:concat([
+              DocKVList,
+              [
+                %% {<<"_id">>, Id},
+                %% {<<"_rev">>, Rev1},
+                {<<"fileKey">>, list_to_binary(FileKey)},
+                {<<"fieldName">>, FieldName},
+                {<<"filename">>, Filename},
+                {<<"ctype">>, CType},
+                {<<"ctransferEncoding">>, CTransferEncoding}
+              ]
+            ])},
+
+            {ok, ResultDoc} = couch:save("user_uploads", Doc),
+
+            FileList1 = lists:append([{Filename, ResultDoc}], FileList),
 
             {ok, Req3, FieldList, FileList1}
         end,
-      multipart(Req4, NewFieldList, NewFileList);
+      multipart(UKey, Req4, NewFieldList, NewFileList);
     {done, Req2} ->
       {ok, Req2, FieldList, FileList}
   end.
 
-stream_file(Req) ->
-  {A, B, C} = now(),
-  Rand = binary_to_list(base64:encode(binary_to_list(crypto:strong_rand_bytes(10)))),
-  Fname = lists:flatten(io_lib:format("~p.~p.~p.~s.tmp", [A, B, C, Rand])),
-  stream_file(Req, list_to_binary(Fname)).
-
-stream_file(Req, Fname) ->
+stream_file(Req, UploadRef) ->
   case cowboy_req:part_body(Req) of
     {ok, Body, Req2} ->
-      file:write_file(Fname, Body, [append, raw, binary]),
-      {ok, Fname, Req2};
+      ok = couch:stream_attach(UploadRef, Body),
+      {ok, Result} = couch:stream_attach_done(UploadRef),
+      {ok, Result, Req2};
     {more, Body, Req2} ->
-      file:write_file(Fname, Body, [append, raw, binary]),
-      stream_file(Req2, Fname)
+      ok = couch:stream_attach(UploadRef, Body),
+      stream_file(Req2, UploadRef)
   end.
