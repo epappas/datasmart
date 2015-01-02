2baa03f01ffcc3bc8a96782c9d856496461e6773
Evangelos Pappas <evangelos.pappas@matchesfashion.com>
2015-01-02 11:01:05 +0000

    devops depends of datasmart_erl

--- devops/chef-repo/cookbooks/datasmart_erl/metadata.rb
+++ devops/chef-repo/cookbooks/datasmart_erl/metadata.rb
@@ -5,3 +5,11 @@ license          'All rights reserved'
 description      'Installs/Configures datasmart_erl'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'build-essential'
+depends 'openssl'
+depends 'erlang'
+depends 'couchdb'
+depends 'nodejs'
+depends 'redisio'
+depends 'chef-client'
