e0a21fe0710309a7fff05e704a41512135702a9d
Evangelos Pappas <evangelos.pappas@matchesfashion.com>
2015-01-02 10:42:28 +0000

    devops chef

---
+++ devops/chef-repo/cookbooks/datasmart_erl/metadata.rb
@@ -0,0 +1,7 @@
+name             'datasmart_erl'
+maintainer       'evalonlabs'
+maintainer_email 'epappas@evalonlabs.com'
+license          'All rights reserved'
+description      'Installs/Configures datasmart_erl'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ devops/chef-repo/cookbooks/datasmart_erl/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: datasmart_erl
+# Recipe:: default
+#
+# Copyright 2015, evalonlabs
+#
+# All rights reserved - Do Not Redistribute
+#
