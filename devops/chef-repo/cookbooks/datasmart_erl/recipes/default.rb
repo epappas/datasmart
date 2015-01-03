18f8b4cd67a7d092a26ccec02425b88a57e7a564
Evangelos Pappas <evangelos.pappas@matchesfashion.com>
2015-01-03 11:16:52 +0000

    decouple devops

--- devops/chef-repo/cookbooks/datasmart_erl/recipes/default.rb
+++
@@ -1,8 +0,0 @@
-#
-# Cookbook Name:: datasmart_erl
-# Recipe:: default
-#
-# Copyright 2015, evalonlabs
-#
-# All rights reserved - Do Not Redistribute
-#
--- devops/chef-repo/solo.rb
+++
@@ -1,3 +0,0 @@
-home = File.expand_path('/app/devops')
-file_cache_path File.join(home, 'chef-solo')
-cookbook_path File.join(home, 'chef-repo', 'cookbooks')
